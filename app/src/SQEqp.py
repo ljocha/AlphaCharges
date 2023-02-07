60326e19d9f46da4ee328add91e82ed8671c80e5
Ales Krenek <ljocha@ics.muni.cz>
2023-02-07 14:01:16 +0000

    orig32

--- app/src/SQEqp.py
+++ app/src/SQEqp.py
@@ -19,6 +19,13 @@ def calculate_charges(molecule,impl='orig'):
                               molecule.coordinates,
                               molecule.total_chg,
                               molecule.precalc_params)
+    elif impl == 'orig32':
+        charges = orig32_sqeqp_calculate(molecule.bonds,
+                              molecule.precalc_bond_hardnesses,
+                              molecule.coordinates,
+                              molecule.total_chg,
+                              molecule.precalc_params)
+
     elif impl == 'new':
         charges = new_sqeqp_calculate(molecule.bonds,
                               molecule.precalc_bond_hardnesses,
@@ -165,6 +172,46 @@ def triang_sqeqp_calculate(bonds,
     B_sqe = np.dot(T, electronegativities)
     r = np.dot(np.linalg.solve(A_sqe, B_sqe), T) + initial_charges
     return r
+
+@jit(nopython=True, cache=True)
+def orig32_sqeqp_calculate(bonds,
+                    precalc_bond_hardnesses,
+                    _coordinates,
+                    total_chg,
+                    precalc_params):
+    # this method is the same for both SQEqp and SQEqps
+    coordinates = _coordinates.astype(np.float32)
+    electronegativities = precalc_params[:, 0]
+    hardnesses = precalc_params[:, 1]
+    radiuses = precalc_params[:, 2].astype(np.float32)
+    initial_charges = precalc_params[:, 3].astype(np.float32)
+    num_of_ats = len(coordinates)
+    num_of_bonds = len(bonds)
+    T = np.zeros((num_of_bonds, num_of_ats), dtype=np.float32)
+    matrix = np.empty((num_of_ats, num_of_ats), dtype=np.float32)
+    for i,(a1, a2, _) in enumerate(bonds):
+        T[i, a1] += 1
+        T[i, a2] -= 1
+    for i in range(num_of_ats):
+        matrix[i, i] = hardnesses[i]
+        i_radius = radiuses[i]
+        ix, iy, iz = coordinates[i]
+        for j, (j_radius, (jx,jy,jz)) in enumerate(zip(radiuses[i+1:],
+                                                       coordinates[i+1:]),
+                                                       i + 1):
+            d0 = np.sqrt(i_radius + j_radius)
+            distance = np.sqrt((ix - jx) ** 2 + (iy - jy) ** 2 + (iz - jz) ** 2)
+            matrix[i, j] = matrix[j, i] = erf(distance / d0) / distance
+    initial_charges -= (np.sum(initial_charges) - total_chg) / len(initial_charges)
+    A_sqe = np.dot(T, np.dot(matrix, T.T))
+    for i, hardness in enumerate(precalc_bond_hardnesses):
+        A_sqe[i, i] += hardness
+    electronegativities -= np.dot(matrix, initial_charges)
+    electronegativities += hardnesses * initial_charges
+    B_sqe = np.dot(T, electronegativities)
+    r = np.dot(np.linalg.solve(A_sqe, B_sqe), T) + initial_charges
+    return r
+
 
 @jit(nopython=True, cache=True)
 def orig_sqeqp_calculate(bonds,
